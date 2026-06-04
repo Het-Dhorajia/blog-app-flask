f5f43f77b9ac311bb3d3b658c0ca62e8b1289f8b
Het-Dhorajia <hetdhorajia21@gmail.com>
2026-06-04 18:36:24 +0530

    database connected to app.py

--- app.py
+++ app.py
@@ -1,21 +1,68 @@
-from flask import Flask,  render_template
+from flask import Flask,   redirect, render_template , request,session ,flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = "mysecretkey"
 
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model ):
+    sno = db.Column(db.Integer, primary_key=True)
+    Username = db.Column(db.String(200), nullable=False)
+    Password = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+@app.route('/' , methods=['GET', 'POST'])
 def home():
 
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
 
-    return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-@app.route('/signup')
+        user = User.query.filter_by(Username=username).first()
+
+        if user and user.Password == password:
+            session['user'] = username
+            return redirect('/dashboard')
+
+        else:
+            flash("Invalid username or password")
+            return redirect('/')
+
+    return render_template('auth/login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-    return render_template('signup.html')
+        new_user = User(Username=username, Password=password)
+        db.session.add(new_user)
+        db.session.commit()
 
+        return redirect('login')
+
+    return render_template('auth/signup.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user' in session:
+        return render_template('dashboard.html')
+    else:
+        return redirect('/login')
+    
+
+with app.app_context():
+    db.create_all()
 
 app.run(debug=True , port=5001)
